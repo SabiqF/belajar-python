c87fee82af1f6ff6a286a5d9b564ce0e97de220e
belajar_python <sabiqfauzil@gmail.com>
2024-03-30 16:32:49 +0700

    modified:   latihan4.py new file:   tugas dasar pemrograman/latihan2.py

--- latihan4.py
+++ latihan4.py
@@ -1,11 +1,17 @@
-print("data diri Mahasiswa")
-nim = input("NIM : ")
-nama = input("Nama : ")
-jurusan = input("Jurusan : ")
-alamat = input("Alamat : ")
+var1 = "Hello World"
+var2 = "I love Python"
+print("var1[0]", var1[1])
+print("var2[5:6]", var2[1])
 
-print("hasil cetak data diatas adalah")
-print("Nim :" + nim)
-print("Nama :" + nama)
-print("Jurusan :" + jurusan)
-print("alamat :" + alamat)
+str1 = "hello"
+str2 = "python"
+print(str1 + " " + str2)
+print(len(str1))
+
+print('''kemarin hari jum'at''')
+print('kemarin hari jum\'at')
+
+str4 = ('kemarin hari jum\'at')
+print(len(str4))
+
+print("ini adalah baris atas \n dan ini baris kedua")
---
+++ tugas dasar pemrograman/latihan2.py
@@ -0,0 +1,16 @@
+print("TOKO MAINAN ANAK".center(50))
+print("**********************".center(50))
+
+nama_pembeli = input("Masukan nama pembeli : ")
+kode_mainan = input("Masukan kode mainan : ")
+harga = int(input("Masukan harga : "))
+jumlah_beli = int(input("Masukan jumlah beli : "))
+total_harga = harga * jumlah_beli
+
+print("===============================")
+
+print("Nama Pembeli = ", nama_pembeli)
+print("Kode Mainan = ", kode_mainan)
+print("Harga = ", harga)
+print("Jumlah Beli = ", jumlah_beli)
+print("Total = ", total_harga)
